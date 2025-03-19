a11a0fb791902b9b61ba926d7330778bbe3254c2
Mateus Abreu <mateusabreucn@gmail.com>
2025-03-19 09:01:38 -0300

    init: initial commit

---
+++ core/index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const DB_PATH_FILE = "./core/db";
+
+console.log("[CRUD]");
+
+function create(content) {
+  fs.writeFileSync(DB_PATH_FILE, content);
+  return content;
+}
+
+console.log(create("Hello World!"));
